4d2fbb23b6ea9187d87a89fcc65c20e2746fb50c
Nicory <pedronicory@gmail.com>
2023-10-02 01:00:38 -0300

    trying to do someting

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,45 +1,64 @@
-import { TextInput, TouchableOpacity, View, Text, Alert, FlatList } from "react-native";
+import {
+    TextInput,
+    TouchableOpacity,
+    View,
+    Text,
+    Alert,
+    FlatList,
+    Keyboard,
+} from "react-native";
 import { styles } from "./styles";
 import { useState } from "react";
-import Icons from '@expo/vector-icons/Ionicons'
-import Checkbox from 'expo-checkbox'
+import Icons from "@expo/vector-icons/Ionicons";
+import Checkbox from "expo-checkbox";
 
 export function HomeScreen() {
+    const [todoList, setTodoList] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+    const [checkeds, setCheckeds] = useState(0);
 
-
-    // { name: "PHP" },
-    // { name: "Java" },
-    // { name: "JavaScript" },
-    // { name: "Python" },
-
-
-    const [todoList, setTodoList] = useState([])
-    const [inputValue, setInputValue] = useState('')
-
+    function removeItem(id) {
+        if (todoList.length > 1) {
+            setTodoList(todoList.slice(todoList.indexOf(id.target, 1)));
+        } else {
+            setTodoList([]);
+        }
+    }
 
     function addItem() {
         if (!inputValue.trim()) return;
 
-        const existItem = !!todoList.find(todo => todo.name === inputValue)
+        const existItem = !!todoList.find((todo) => todo.name === inputValue);
 
-        if (existItem){
-            Alert.alert('ops', 'ja existe')
-            return
+        if (existItem) {
+            Alert.alert("Ops..", "Parece que já tem um desse em :/");
+            return;
         }
 
         setTodoList((prev) => {
-            return [...prev, { name: inputValue }]
-        })
+            setInputValue("");
+            -Keyboard.dismiss();
+            return [...prev, { name: inputValue }];
+        });
     }
 
     return (
-        // TITLE 
+        // TITLE
         <View style={styles.container}>
             <View style={styles.titleConteiner}>
                 <Text style={styles.title}>Lista de Tarefas</Text>
                 <View style={styles.checkConteiner}>
-                    <Text style={styles.checkCountText}>0/{todoList.length}</Text>
-                    {/* <Icons style={styles.checkCountIcon} name='checkmark-outline' /> */}
+                    {todoList.length > 0 ? (
+                        <Text style={styles.checkCountText}>
+                            {checkeds}/{todoList.length}
+                            <Icons
+                                style={styles.checkCountIcon}
+                                name="checkmark-outline"
+                            />
+                        </Text>
+                    ) : (
+                        null
+                    )}
                 </View>
             </View>
 
@@ -47,10 +66,12 @@ export function HomeScreen() {
             <View style={styles.inputContainer}>
                 <TextInput
                     placeholder="Nome da tarefa"
-                    placeholderTextColor='#71717A'
+                    placeholderTextColor="#71717A"
                     style={styles.input}
                     onChangeText={(txt) => setInputValue(txt)}
-                    value={inputValue} />
+                    value={inputValue}
+                    onSubmitEditing={addItem}
+                />
                 <TouchableOpacity
                     style={styles.buttonAdd}
                     onPress={addItem}
@@ -62,53 +83,43 @@ export function HomeScreen() {
                 </TouchableOpacity>
             </View>
 
-            {/* LIST PART */}
-            {/* <View style={styles.list}>
-                {todoList.map(item => (
-                    <View key={item.name} style={styles.listItem}>
-                        <Text style={styles.listItemText}>{item.name}</Text>
-                        <TouchableOpacity
-                            style={styles.buttonRemove}
-                            onPress={addItem}
-                            name="add"
-                            size={32}
-                            color="#fff"
-                        >
-                            <Icons style={styles.iconRemove} name="remove-circle-outline" />
-                        </TouchableOpacity>
-                    </View>
-                ))}
-            </View> */}
-
             <FlatList
                 data={todoList}
                 style={styles.list}
-                contentContainerStyle={styles.listContentStyle} 
+                contentContainerStyle={styles.listContentStyle}
                 ListEmptyComponent={
                     <View style={styles.emptyListContainer}>
-                        <Icons name="trash-bin-outline" style={styles.emptyListIcon} />
+                        <Icons
+                            name="trash-bin-outline"
+                            style={styles.emptyListIcon}
+                        />
                         <Text style={styles.emptyListTitle}>Lista Vazia</Text>
-                        <Text style={styles.emptyListDescrption}>Cadastre alguma atividade</Text>
+                        <Text style={styles.emptyListDescrption}>
+                            Cadastre alguma atividade
+                        </Text>
                     </View>
                 }
-                renderItem={({item}) => (
+                renderItem={({ item }) => (
                     <View style={styles.listItem}>
                         <TouchableOpacity style={styles.checkboxConteiner}>
-                            <Checkbox
-                                value={false}
-                                onPress={() => { }}
-                            />
+                            <Checkbox value={false} onPress={() => {}} />
                             <Text style={styles.listItemText}>{item.name}</Text>
                         </TouchableOpacity>
 
-                        <TouchableOpacity>
-                            <Icons name="trash-outline" style={styles.listItemButtonRemove} />
+                        <TouchableOpacity
+                            onPress={() => {
+                                removeItem(item.name);
+                            }}
+                        >
+                            <Icons
+                                name="trash-outline"
+                                style={styles.listItemButtonRemove}
+                            />
                         </TouchableOpacity>
                     </View>
                 )}
-
                 ItemSeparatorComponent={<View style={{ padding: 10 }}></View>}
             />
         </View>
-    )
+    );
 }
